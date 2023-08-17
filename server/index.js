3b1a9e2af163c67068fdfa718ad5493f8f855bdc
Sanchay Kumar <sanchaykumar9@gmail.com>
2023-08-17 19:34:31 +0530

    Update cors origin

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = socketio(server , {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 })
